cb1c66c124ac92bd796c88a954a30969d1a498ec
Robin Vivant <robin.vivant@gmail.com>
2014-12-19 17:25:13 +0100

    COLOOOORS

--- Server/client/pages/slave/slave.js
+++ Server/client/pages/slave/slave.js
@@ -16,6 +16,25 @@ Template.slave.events({
 
 Template.slave.created = function(){
   Meteor.subscribe('samples');
+
+  // SWAG
+  setInterval(function(){
+    var date = new Date();
+    var h = date.getHours();
+    var m = date.getMinutes();
+    var s = date.getSeconds();
+    if((''+h).length == 1) h = '0'+h;
+    if((''+s).length == 1) s = '0'+s;
+    if((''+m).length == 1) m = '0'+m;
+
+    $('body').velocity({
+      properties:{
+        backgroundColor: '#'+h+m+s
+      }, options:{
+        duration:'900'
+      }
+    });//css('background-color', '#'+h+m+s);
+  }, 1000)
 };
 
 Template.slave.destroyed = function(){
